af17c254ff50efe0e21a775a075850cd291d3bef
Brendan J. Herger <b@revup.com>
2017-12-15 14:55:41 -0800

    Python environment and interface for script

--- bin/main.py
+++ bin/main.py
@@ -28,6 +28,10 @@ def main():
 def extract():
     logging.info('Begin extract')
 
+    # TODO Check if Niezsche is available. If not, download.
+
+    # TODO Create observations DataFrame
+
     lib.archive_dataset_schemas('extract', locals(), globals())
     logging.info('End extract')
     pass
@@ -35,6 +39,14 @@ def extract():
 
 def transform():
     logging.info('Begin transform')
+
+    # TODO Filter characters
+
+    # TODO Create character ngrams
+
+    # TODO Convert character ngrams into label representations
+
+    # TODO Create y, containing next character after window
 
     lib.archive_dataset_schemas('transform', locals(), globals())
     logging.info('End transform')
@@ -44,6 +56,12 @@ def transform():
 def model():
     logging.info('Begin model')
 
+    # TODO Create embedding
+
+    # TODO Create model architecture
+
+    # TODO Train model
+
     lib.archive_dataset_schemas('model', locals(), globals())
     logging.info('End model')
     pass
@@ -51,6 +69,12 @@ def model():
 
 def load():
     logging.info('Begin load')
+
+    # TODO Serialize encoder
+
+    # TODO Serialize model
+
+
 
     lib.archive_dataset_schemas('load', locals(), globals())
     logging.info('End load')
