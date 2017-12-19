43e64184b9d4bdbcbe40aca4e84cdfa1d05bb3ba
Brendan J. Herger <b@revup.com>
2017-12-19 09:05:00 -0800

    Using transform for generation

--- bin/main.py
+++ bin/main.py
@@ -47,71 +47,70 @@ def extract():
 
     return text
 
+
 def transform(text):
-    chars = sorted(list(set(text)))
-    print('total chars:', len(chars))
+    # TODO Should use universal character set, for inference time
+    chars = sorted(list(set(lib.legal_characters())))
+
+    text = map(lambda x: x.lower(), text)
+    text = filter(lambda x: x in lib.legal_characters(), text)
+    text = ''.join(text)
+
     char_indices = dict((c, i) for i, c in enumerate(chars))
     indices_char = dict((i, c) for i, c in enumerate(chars))
 
     # cut the text in semi-redundant sequences of maxlen characters
-    maxlen = 40
     step = 3
     sentences = []
     next_chars = []
-    for i in range(0, len(text) - maxlen, step):
-        sentences.append(text[i: i + maxlen])
-        next_chars.append(text[i + maxlen])
-    print('nb sequences:', len(sentences))
+    for observation_index in range(0, len(text) - lib.get_conf('ngram_len'), step):
+        sentences.append(text[observation_index: observation_index + lib.get_conf('ngram_len')])
+        next_chars.append(text[observation_index + lib.get_conf('ngram_len')])
 
-    print('Vectorization...')
-    x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+    x = np.zeros((len(sentences), lib.get_conf('ngram_len'), len(chars)), dtype=np.bool)
     y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
-    for i, sentence in enumerate(sentences):
+    for observation_index, sentence in enumerate(sentences):
         for t, char in enumerate(sentence):
-            x[i, t, char_indices[char]] = 1
-        y[i, char_indices[next_chars[i]]] = 1
+            x[observation_index, t, char_indices[char]] = 1
+        y[observation_index, char_indices[next_chars[observation_index]]] = 1
 
     return text, char_indices, indices_char, x, y
 
 def model(text, char_indices, indices_char, x, y):
-    maxlen = 40
-    chars = sorted(list(set(text)))
 
-    # TODO Model
-    print('Build model...')
+    chars = sorted(list(set(lib.legal_characters())))
+
     model = Sequential()
-    model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+    model.add(LSTM(128, input_shape=(lib.get_conf('ngram_len'), len(chars))))
     model.add(Dense(len(chars)))
     model.add(Activation('softmax'))
 
     optimizer = RMSprop(lr=0.01)
     model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
-    # train the model, output generated text after each iteration
+    # Train the model, output generated text after each iteration
     for iteration in range(1, 60):
-        print()
-        print('-' * 50)
-        print('Iteration', iteration)
+        logging.info('Interation number: {}'.format(iteration))
         model.fit(x, y,
                   batch_size=128,
                   epochs=1)
 
-        start_index = random.randint(0, len(text) - maxlen - 1)
+        start_index = random.randint(0, len(text) - lib.get_conf('ngram_len') - 1)
 
         for diversity in [0.2, 0.5, 1.0, 1.2]:
-            print()
-            print('----- diversity:', diversity)
 
             generated = ''
-            sentence = text[start_index: start_index + maxlen]
+            sentence = text[start_index: start_index + lib.get_conf('ngram_len')+1]
             generated += sentence
             print('----- Generating with seed: "' + sentence + '"')
             sys.stdout.write(generated)
 
-            for i in range(400):
-                x_pred = np.zeros((1, maxlen, len(chars)))
-                for t, char in enumerate(sentence):
-                    x_pred[0, t, char_indices[char]] = 1.
+
+            for next_char_index in range(400):
+            #     x_pred = np.zeros((1, lib.get_conf('ngram_len'), len(chars)))
+            #     for t, char in enumerate(sentence):
+            #         x_pred[0, t, char_indices[char]] = 1.
+                text_text, text_char_indices, text_indices_char, x_pred, text_y = transform(sentence)
 
                 preds = model.predict(x_pred, verbose=0)[0]
                 next_index = sample(preds, diversity)
